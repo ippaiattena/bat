5578c3b44853af5d7d4399cc17d1d25c19fbedac
ippaiattena <ippaiattena@outlook.com>
2015-06-17 22:54:28 +0900

    add file
    clear.bat add

---
+++ clear.bat
@@ -0,0 +1,25 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set ODIR=C:\Users\null\Capture
+set IDIR=C:\Users\null\Videos
+
+for /f "usebackq" %%i in (`dir %ODIR% /B *.mp4`) do (
+set IFILE=%%i
+del %ODIR%\!IFILE!
+)
+
+for /f "usebackq" %%i in (`dir %ODIR% /B *.jpg`) do (
+set IFILE=%%i
+del %ODIR%\!IFILE!
+)
+
+for /f "usebackq" %%i in (`dir %IDIR% /B *.wmv`) do (
+set IFILE=%%i
+del %IDIR%\!IFILE!
+)
+
+endlocal
+
+exit
